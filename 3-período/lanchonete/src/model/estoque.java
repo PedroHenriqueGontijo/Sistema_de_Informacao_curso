8bdfbeae5b0cd47c955a1003388de8f01b7d4e15
pedro Henrique Valentin <118783466+PedroHenriqueGontijo@users.noreply.github.com>
2024-03-11 22:22:00 -0300

    Update estoque.java

--- 3-período/lanchonete/src/model/estoque.java
+++ 3-período/lanchonete/src/model/estoque.java
@@ -1,48 +1,59 @@
-package model;
-public class estoque{
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lanchonete_atividade;
 
-    private String qnome;
-    private String qpreco_custo;
-    private int qtd_disponivel;
+/**
+ *
+ * @author host1
+ */
+public class Estoque {
 
+    private String nome_produto;
+    private String quantidade_stoque;
+    private int valor_produto_estoque;
 
-    
-
-    
-
-    
-    public estoque(String qnome, String qpreco_custo, int qtd_disponivel) {
-        this.qnome = qnome;
-        this.qpreco_custo = qpreco_custo;
-        this.qtd_disponivel = qtd_disponivel;
-    }
-    public String getQnome() {
-        return qnome;
-    }
-    public void setQnome(String qnome) {
-        this.qnome = qnome;
-    }
-    public String getQpreco_custo() {
-        return qpreco_custo;
-    }
-    public void setQpreco_custo(String qpreco_custo) {
-        this.qpreco_custo = qpreco_custo;
+    public Estoque(String nome_produto, String quantidade_stoque, int valor_produto_estoque) {
+        this.nome_produto = nome_produto;
+        this.quantidade_stoque = quantidade_stoque;
+        this.valor_produto_estoque = valor_produto_estoque;
     }
 
-
-    public int getQtd_disponivel() {
-        return qtd_disponivel;
+    Estoque() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-
-    public void setQtd_disponivel(int qtd_disponivel) {
-        this.qtd_disponivel = qtd_disponivel;
+    public String getNome_produto() {
+        return nome_produto;
     }
 
+    public void setNome_produto(String nome_produto) {
+        this.nome_produto = nome_produto;
+    }
 
+    public String getQuantidade_stoque() {
+        return quantidade_stoque;
+    }
 
-    
-   
-    
-    
+    public void setQuantidade_stoque(String quantidade_stoque) {
+        this.quantidade_stoque = quantidade_stoque;
+    }
+
+    public int getValor() {
+        return valor_produto_estoque;
+    }
+
+    public void setValor(int valor_produto) {
+        this.valor_produto_estoque = this.valor_produto_estoque;
+    }
+
+    public void info_estoque(String nome_produto, String quantidade_stoque, int valor_produto) {
+        this.nome_produto = nome_produto;
+        this.quantidade_stoque = quantidade_stoque;
+        this.valor_produto_estoque = valor_produto;
+       
+    }
+
 }
